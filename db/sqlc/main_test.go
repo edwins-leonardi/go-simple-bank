8fa25ab0a5713538a2a02ac0f7695d5ff6cb3799
Edwins Leonardi <edwins.leonardi>
2025-02-22 15:14:30 +0000

    adding gin web api endpoints

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,21 @@ import (
 	"os"
 	"testing"
 
+	"github.com/edwins-leonardi/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	// Load configuration
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 	// fmt.Println("TestMain")
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/edwins-leonardi/simplebank/api"
+	db "github.com/edwins-leonardi/simplebank/db/sqlc"
+	"github.com/edwins-leonardi/simplebank/util"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Load configuration
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddr)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
